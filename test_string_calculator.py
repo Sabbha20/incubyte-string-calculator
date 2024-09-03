f2adc45eeb7db32db237e2acd5b8423978385ec6
Sabbha20 <sabbha.power1@gmail.com>
2024-09-03 16:08:44 +0530

    feat: test for negative number exception

--- test_string_calculator.py
+++ test_string_calculator.py
@@ -17,7 +17,20 @@ class TestStringCalculator(unittest.TestCase):
     def test_newline_num_string(self):
         self.assertEqual(add("1\n2,3"), 6)
         self.assertEqual(add("2,3\n4"), 9)
-        
+
     def test_new_delimiter_input(self):
         self.assertEqual(add("//;\n1;2"), 3)
         self.assertEqual(add("//;\n2;3;4;1"), 10)
+
+    def test_negative_num_string(self):
+        with self.assertRaises(ValueError) as ctx:
+            add("3, -4, -5, 6")
+        self.assertEqual(str(ctx.exception),
+                         "negative numbers not allowed -4, -5")
+        with self.assertRaises(ValueError) as ctx:
+            add("//;\n2;-3;4;1")
+        self.assertEqual(str(ctx.exception), "negative numbers not allowed -3")
+        with self.assertRaises(ValueError) as ctx:
+            add("-2\n-3;4;-1")
+        self.assertEqual(str(ctx.exception),
+                         "negative numbers not allowed -2, -3, -1")

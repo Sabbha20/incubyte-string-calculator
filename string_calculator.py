8a22ad3b0d8efe52115e98c556c7d4000646f836
Sabbha20 <sabbha.power1@gmail.com>
2024-09-03 17:02:06 +0530

    fix: handle multiple delimeter in add function

--- string_calculator.py
+++ string_calculator.py
@@ -8,10 +8,15 @@ def add(numbers: str) -> int:
         if numbers.startswith("//"):
             delimiter, numbers = numbers[2:].split("\n", 1)
             if delimiter.startswith("[") and delimiter.endswith("]"):
-                delimiter = delimiter[1:-1]
-            num_lst = numbers.split(delimiter)
-        else:
-            num_lst = re.split(",|\n|;", numbers)
+                # delimiter = delimiter[1:-1]
+                delimiters = re.findall(r"\[(.*?)\]", delimiter)
+                for mydelim in delimiters:
+                    numbers = numbers.replace(mydelim, ",")
+            else:
+                mydelim = delimiter
+                numbers = numbers.replace(mydelim, ",")
+
+        num_lst = re.split(",|\n|;", numbers)
 
     nve_num = [num for num in num_lst if int(num) < 0]
     if nve_num:

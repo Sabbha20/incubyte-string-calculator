9cceaec7cb8313d6a9555547d28da7b2d1d441da
Sabbha20 <sabbha.power1@gmail.com>
2024-09-03 14:33:18 +0530

    feat: add test for single number input

--- test_string_calculator.py
+++ test_string_calculator.py
@@ -5,3 +5,6 @@ from string_calculator import add
 class TestStringCalculator(unittest.TestCase):
     def test_add_empty_string(self):
         self.assertEqual(add(""), 0)
+
+    def test_single_num_string(self):
+        self.assertEqual(add("1"), 1)
